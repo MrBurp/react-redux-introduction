e38e57b34359967cc17ffdb1fc77fbe0a3be02e5
MrBurp <andreass.ziegler@gmail.com>
2019-05-29 11:50:29 +0200

    fixed tree structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
+import Header from "./components/Header";
 
-class Header extends React.Component {
-  render() {
-    return <h1>What to do?</h1>;
-  }
-}
+// class Header extends React.Component {
+//   render() {
+//     return <h1>What to do?</h1>;
+//   }
+// }
 
 render(<Header />, document.querySelector("#main"));
