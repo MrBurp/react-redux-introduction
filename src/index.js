f7d8af68d7070e4ce25aa8ba75112dd7dcf0351d
MrBurp <andreass.ziegler@gmail.com>
2019-06-01 14:21:13 +0200

    Enable localStorage

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-//React
+// React
 import React from "react";
 import { render } from "react-dom";
-//Redux
+
+// Redux
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import appReducer from "./reducers/index";
+import { loadState, saveState } from "./modules/localStorage";
+
 // Components
 import Router from "./components/Router";
 
-//Style
+// Styles
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./sass/style.scss";
+
 // Create Redux store and initialize the app
 const store = createStore(
   appReducer,
+  loadState(),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 render(
   <Provider store={store}>
